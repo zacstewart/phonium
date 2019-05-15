22985ecd2c3f964e6bba736c2bff1f3ad7be914d
Zac Stewart <zgstewart@gmail.com>
2019-05-14 21:05:49 -0400

    Share the RST pin between fona and epd
    This frees up the analog pin for a BUSY

--- main.ino
+++ main.ino
@@ -9,8 +9,8 @@
 #define EPD_CS     10
 #define EPD_DC     9
 #define SRAM_CS    8
-#define EPD_RESET  A0
-#define EPD_BUSY   A1 // can set to -1 to not use a pin (will wait a fixed delay)
+#define EPD_RESET  4
+#define EPD_BUSY   A0 // can set to -1 to not use a pin (will wait a fixed delay)
 #define DISPLAY_ROTATION 2 // Headers to the left
 #define COLOR_BLACK EPD_BLACK
 #define COLOR_GRAY EPD_RED
@@ -23,8 +23,8 @@ byte keys[KEYPAD_ROWS][KEYPAD_COLS] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-byte rowPins[KEYPAD_ROWS] = {3, 2, A2, A3};
-byte colPins[KEYPAD_COLS] = {4, 5, 6, 7};
+byte rowPins[KEYPAD_ROWS] = {A2, A3, A4, A5};
+byte colPins[KEYPAD_COLS] = {A1, 5, 6, 7};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
 
 Adafruit_SSD1608 display(200, 200, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
