3094d92edef6c116198f2000369fe6f2ad6cadfb
Zac Stewart <zgstewart@gmail.com>
2019-06-17 20:45:50 -0400

    Show message count on Home

--- src/Home.cpp
+++ src/Home.cpp
@@ -1,14 +1,28 @@
 #include "Home.h"
 #include "Dialer.h"
 
-Home::Home(Navigator *navigator, Adafruit_SSD1608 *display, Keypad *keypad)
+Home::Home(Navigator *navigator, Adafruit_SSD1608 *display, Keypad *keypad, Adafruit_FONA *fona)
 : navigator(navigator)
 , display(display)
 , keypad(keypad)
+, fona(fona)
 {
 }
 
 void Home::begin() {
+  int8_t numSms = fona->getNumSMS();
+
+  Serial.print(numSms);
+  Serial.println(F(" unread SMSs"));
+
+  display->clearBuffer();
+  display->setCursor(0, 0);
+  display->setTextColor(COLOR_BLACK);
+  display->setTextSize(2);
+  display->println(numSms);
+  display->setTextSize(1);
+  display->print("unread messages");
+  display->display();
 }
 
 void Home::update() {
--- src/Home.h
+++ src/Home.h
@@ -1,7 +1,8 @@
 #ifndef HOME
 #define HOME 0
 
-#include "Adafruit_EPD.h"
+#include <Adafruit_EPD.h>
+#include <Adafruit_FONA.h>
 #include <Keypad.h>
 
 #include "Common.h"
@@ -12,15 +13,15 @@ class Home : public Controller {
   Navigator *navigator;
   Adafruit_SSD1608 *display;
   Keypad *keypad;
+  Adafruit_FONA *fona;
 
   public:
-    Home(Navigator *, Adafruit_SSD1608 *, Keypad *);
+    Home(Navigator *, Adafruit_SSD1608 *, Keypad *, Adafruit_FONA *);
     void begin();
     void update();
 
   private:
     void startDialing(char);
-
 };
 
 #endif
